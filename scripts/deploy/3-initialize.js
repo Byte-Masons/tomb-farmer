668a4feac54173d04c494711248257a82a1b7e4d
tess3rac7 <tess3rac7@protonmail.com>
2022-03-30 22:48:36 -0400

    Post-deployment commit.

--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = 'TODO';
-  const strategyAddress = 'TODO';
+  const vaultAddress = '0xA6423CCF421E591e52124d2E5CD04b00515e6fa2';
+  const strategyAddress = '0x9C661130e2c1c43Fc3108a567d5F5C6Ba5a598B0';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
