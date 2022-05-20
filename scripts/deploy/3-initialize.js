156f4d9bee305acd3ebc9a2feb90d876d60e1832
Magnus Brantheim <m.brantheim@gmail.com>
2022-05-20 10:12:37 +0200

    Deployed ftm-usdc

--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = 'TODO';
-  const strategyAddress = 'TODO';
+  const vaultAddress = '0x024721884f2E6ffc872F5D3C5eB8d8dF9140091D';
+  const strategyAddress = '0x05f87EF8f1Dc92Bd9F0e9D63F91eBC7f673c5393';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
