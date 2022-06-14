71b17f1aebed2868849c044fa5f65ef867979453
Bastien Guyonnet <bastien.guyonnet@tutanota.com>
2022-06-14 15:21:16 +0200

    deployed

--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = 'TODO';
-  const strategyAddress = 'TODO';
+  const vaultAddress = '0xdAB69F11d14f54F08a540748C6748CE0Faa647eF';
+  const strategyAddress = '0x8a36f97BEDC02cb68CB3aD684A408a832E7bBf95';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
