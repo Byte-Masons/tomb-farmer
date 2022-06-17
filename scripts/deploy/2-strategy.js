91c8e77dbfac682166747d560e284f3b3b6e3309
Bastien Guyonnet <bastien.guyonnet@tutanota.com>
2022-06-17 21:00:45 +0200

    tshare-usdc-v2 deployed

--- scripts/deploy/2-strategy.js
+++ scripts/deploy/2-strategy.js
@@ -1,7 +1,7 @@
 const hre = require('hardhat');
 
 async function main() {
-  const vaultAddress = '0xCa491C9F4807f6c334425e96c8dF0835a3eA7813';
+  const vaultAddress = '0xB866fC187fFC578b6f720387dF2089a9D6C95Ada';
 
   const Strategy = await ethers.getContractFactory('ReaperStrategyTombWftmUnderlying');
   const treasuryAddress = '0x0e7c5313E9BB80b654734d9b7aB1FB01468deE3b';
@@ -10,8 +10,8 @@ async function main() {
   const strategist2 = '0x81876677843D00a7D792E1617459aC2E93202576';
   const strategist3 = '0x1A20D7A31e5B3Bc5f02c8A146EF6f394502a10c4';
 
-  const wantAddress = '0x8C853ce1561A2c2cD2E857670e3cCd04BA4cB27b';
-  const poolId = 10;
+  const wantAddress = '0xDEc1259188E6c5273AcD1e84d5B4b58897CA013e';
+  const poolId = 6;
 
   const strategy = await hre.upgrades.deployProxy(
     Strategy,
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = '0xCa491C9F4807f6c334425e96c8dF0835a3eA7813';
-  const strategyAddress = '0x80BD129328d56AdeaEEBCa4e4eA7e428D5DD587f';
+  const vaultAddress = '0xB866fC187fFC578b6f720387dF2089a9D6C95Ada';
+  const strategyAddress = '0xA886c625B7b3b1c6B3D1162d31f4a28ccbAA5ff8';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
