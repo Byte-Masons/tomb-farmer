6d11cf47f3020b87605b8962f6f3aa9c228563c6
Bastien Guyonnet <bastien.guyonnet@tutanota.com>
2022-06-20 10:57:49 +0200

    deployed usdc-mim-v2

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -1,10 +1,10 @@
 async function main() {
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
 
-  const wantAddress = '0x8C853ce1561A2c2cD2E857670e3cCd04BA4cB27b';
-  const tokenName = 'FTM-USDC Tomb Crypt';
-  const tokenSymbol = 'rf-FTM-USDC';
-  const depositFee = 0;
+  const wantAddress = '0xd840aF68b35469eC3478c9b0CBCDdc6dc80Dd98C';
+  const tokenName = 'USDC-MIM Tomb Crypt';
+  const tokenSymbol = 'rf-USDC-MIM';
+  const depositFee = 25;
   const tvlCap = ethers.constants.MaxUint256;
 
   const vault = await Vault.deploy(wantAddress, tokenName, tokenSymbol, depositFee, tvlCap);
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = '0xCa491C9F4807f6c334425e96c8dF0835a3eA7813';
-  const strategyAddress = '0x80BD129328d56AdeaEEBCa4e4eA7e428D5DD587f';
+  const vaultAddress = '0x96592E8c8534656642dBbD22403a4fbdaA2E3fCa';
+  const strategyAddress = '0x30BC269d535298Dd515cb31B4E7769efd0B1D815';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
